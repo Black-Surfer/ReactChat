0074c0b4e175e0f4975ba5734770e62a4d515c92
Giwa Uthman A <giwauthman8@gmail.com>
2022-10-24 19:17:43 +0100

    Configure persistent state

---
+++ src/config/presist-redux.js
@@ -0,0 +1,31 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { createStateSyncMiddleware, initStateWithPrevTab } from 'redux-state-sync'
+import chat from "../reducers/index";
+
+const initialState = {};
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const middlewares = [
+  createStateSyncMiddleware(persistConfig),
+]
+
+const persistedReducer = persistReducer(persistConfig, chat);
+
+export default function presist() {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  let store = createStore(
+    persistedReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(...middlewares))
+  );
+  initStateWithPrevTab(store)
+  let persistor = persistStore(store);
+  console.log(store, "new");
+  return { store, persistor };
+}
