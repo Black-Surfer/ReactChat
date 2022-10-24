5c124265583adabe1e1c8244c5c57272a6b5b413
Giwa Uthman A <giwauthman8@gmail.com>
2022-10-24 19:24:02 +0100

    integrate redux state management with app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { PersistGate } from 'redux-persist/integration/react';
+import presist from './config/presist-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = presist();
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <Provider store={store.store}>
+    <PersistGate loading={null} persistor={store.persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+)
+
+registerServiceWorker()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
